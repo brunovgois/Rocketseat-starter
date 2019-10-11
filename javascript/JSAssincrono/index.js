fdecb7a0f3dd558af8374769d3b5eb32407cf8c1
Bruno Gois <brunovgois@gmail.com>
2019-10-11 14:10:39 -0300

    exercicio 1

--- javascript/JSAssincrono/index.js
+++ javascript/JSAssincrono/index.js
@@ -1,12 +1,16 @@
 function checaIdade(idade) {
-	// Retornar uma promise
- }
- 
- checaIdade(20)
-	.then(function() {
-	console.log("Maior que 18");
-	})
-	.catch(function() {
-	console.log("Menor que 18");
-	});
- 
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (idade >= 18) resolve();
+      else reject();
+    }, 2000);
+  });
+}
+
+checaIdade(20)
+  .then(function() {
+    console.log("Maior que 18");
+  })
+  .catch(function() {
+    console.log("Menor que 18");
+  });
